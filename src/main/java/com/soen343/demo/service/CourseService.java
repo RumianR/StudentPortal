ee134195bd1c93a17c5d4a723144ba510252b250
Naimur Rashid <RashidN96@gmail.com>
2019-11-27 14:17:43 -0500

    implemented add and remove courses functionality

--- src/main/java/com/soen343/demo/service/CourseService.java
+++ src/main/java/com/soen343/demo/service/CourseService.java
@@ -25,4 +25,6 @@ public class CourseService {
         courseRepository.findAll().forEach(courses::add);
         return courses;
     }
+
+
 }
--- src/main/java/com/soen343/demo/service/UserService.java
+++ src/main/java/com/soen343/demo/service/UserService.java
@@ -1,33 +1,36 @@
 package com.soen343.demo.service;
 
 
+import com.soen343.demo.model.Course;
 import com.soen343.demo.model.Role;
 import com.soen343.demo.model.User;
 import com.soen343.demo.repository.RoleRepository;
 import com.soen343.demo.repository.UserRepository;
+import com.soen343.demo.repository.CourseRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 @Service("userService")
 public class UserService {
 
     private UserRepository userRepository;
     private RoleRepository roleRepository;
+    private CourseRepository courseRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     public UserService(UserRepository userRepository,
                        RoleRepository roleRepository,
+                       CourseRepository courseRepository,
                        BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.courseRepository = courseRepository;
     }
 
     public User findUserByEmail(String email) {
@@ -46,6 +49,40 @@ public class UserService {
         user.setActive(1);
         Role userRole = roleRepository.findByRole("ADMIN");
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        userRepository.save(user);
+    }
+
+
+    public void addCourses(User user, String[] CoursesRequest) {
+
+
+        int[] courseIds = new int[CoursesRequest.length];
+        for (int i = 0; i < CoursesRequest.length; i++) {
+            courseIds[i] = Integer.parseInt(CoursesRequest[i]);
+        }
+
+        List<Course> courseList = courseRepository.findByIdIn(courseIds);
+        Set<Course> currentCourses = user.getCourses();
+        currentCourses.addAll(courseList);
+        user.setCourses(currentCourses);
+
+        userRepository.save(user);
+    }
+
+
+    public void removeCourses(User user, String[] CoursesRequest) {
+
+
+        int[] courseIds = new int[CoursesRequest.length];
+        for (int i = 0; i < CoursesRequest.length; i++) {
+            courseIds[i] = Integer.parseInt(CoursesRequest[i]);
+        }
+
+        List<Course> courseList = courseRepository.findByIdIn(courseIds);
+        Set<Course> currentCourses = user.getCourses();
+        currentCourses.removeAll(courseList);
+        user.setCourses(currentCourses);
+
         userRepository.save(user);
     }
 
