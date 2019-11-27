b2a7ea9c1b129e7c8569839e0e3a167e3bb7bfd3
Naimur Rashid <RashidN96@gmail.com>
2019-11-26 19:46:23 -0500

    fixed typo

--- src/main/java/com/soen343/demo/repository/CourseRepository.java
+++ src/main/java/com/soen343/demo/repository/CourseRepository.java
@@ -3,7 +3,7 @@ import com.soen343.demo.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository("userRepository")
+@Repository("courseRepository")
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
