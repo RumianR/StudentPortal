cd56cf79204770f19b8484d0ef0b850852c5c6c6
Divesh Patel <divesh.pat3l@gmail.com>
2019-11-27 13:06:48 -0500

    Added Enroll checkbox

--- src/main/java/com/soen343/demo/controller/LoginController.java
+++ src/main/java/com/soen343/demo/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -26,7 +27,7 @@ public class LoginController extends BaseController {
         ModelAndView modelAndView = new ModelAndView();
         User user = new User();
         modelAndView.addObject("user", user);
-        modelAndView.setViewName("registration.jsp");
+        modelAndView.setViewName("registration");
         return modelAndView;
     }
 
@@ -40,12 +41,12 @@ public class LoginController extends BaseController {
                             "There is already a user registered with the email provided");
         }
         if (bindingResult.hasErrors()) {
-            modelAndView.setViewName("registration.jsp");
+            modelAndView.setViewName("registration");
         } else {
             userService.saveUser(user);
             modelAndView.addObject("successMessage", "User has been registered successfully");
             modelAndView.addObject("user", new User());
-            modelAndView.setViewName("registration.jsp");
+            modelAndView.setViewName("registration");
 
         }
         return modelAndView;
@@ -70,10 +71,18 @@ public class LoginController extends BaseController {
     }
 
     @RequestMapping(value="/enroll/addcourse", method = RequestMethod.GET)
-    public ModelAndView addcourse(){
+    public ModelAndView addcoursePage(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("courses", courseService.listAll());
         modelAndView.setViewName("/course/addcourse");
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/enroll/addcourse", method = RequestMethod.POST)
+    public ModelAndView addcourse(@RequestParam("chicken") String[] checkboxValue){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("checkboxvalues", checkboxValue.length);
+        modelAndView.setViewName("/test");
         return modelAndView;
     }
 
