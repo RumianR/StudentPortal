38c9d5bd0f58ed2de2188d585fd669865f7d8a90
Naimur Rashid <RashidN96@gmail.com>
2019-11-27 20:39:54 -0500

    trying

--- src/main/java/com/soen343/demo/controller/LoginController.java
+++ src/main/java/com/soen343/demo/controller/LoginController.java
@@ -175,7 +175,7 @@ public class LoginController extends BaseController {
         return modelAndView;
     }
 
-    @CrossOrigin
+    @CrossOrigin(origins = "*")
     @RequestMapping("/greeting")
     public ModelAndView test(@RequestParam("courseid") String courseid){
         ModelAndView modelAndView = new ModelAndView();
