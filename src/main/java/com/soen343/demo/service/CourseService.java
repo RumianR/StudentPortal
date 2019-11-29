6342d261680dc0c5c01873efb2982618db96b0f8
Naimur Rashid <RashidN96@gmail.com>
2019-11-28 21:17:02 -0500

    changed time

--- src/main/java/com/soen343/demo/service/CourseService.java
+++ src/main/java/com/soen343/demo/service/CourseService.java
@@ -30,6 +30,61 @@ public class CourseService {
     }
 
     public Course saveCourse(Course course) {
+
+        String time = "";
+        String[] userSubmittedTime = course.getTime().split(",");
+        if(userSubmittedTime.length==3){
+            course.setTimeStart(userSubmittedTime[1]);
+            course.setTimeEnd(userSubmittedTime[2]);
+
+                if(userSubmittedTime[0].contains("Mo")){
+                   time+= "Mo";
+                }
+                if(userSubmittedTime[0].contains("Tu")){
+                time += "Tu";
+                }
+                if(userSubmittedTime[0].contains("We")){
+                time += "We";
+                }
+                if(userSubmittedTime[0].contains("Th")){
+                time += "Th";
+                }
+                if(userSubmittedTime[0].contains("Fr")){
+                time += "Fr";
+                }
+
+            time += " " + userSubmittedTime[1]+ " - " + userSubmittedTime[2];
+
+        }
+
+        else if(userSubmittedTime.length==4){
+            course.setTimeStart(userSubmittedTime[2]);
+            course.setTimeEnd(userSubmittedTime[3]);
+
+            for (int i=0; i<2 ; i++){
+
+                if(userSubmittedTime[i].contains("Mo")){
+                    time+= "Mo";
+                }
+                if(userSubmittedTime[i].contains("Tu")){
+                    time += "Tu";
+                }
+                if(userSubmittedTime[i].contains("We")){
+                    time += "We";
+                }
+                if(userSubmittedTime[i].contains("Th")){
+                    time += "Th";
+                }
+                if(userSubmittedTime[i].contains("Fr")){
+                    time += "Fr";
+                }
+            }
+
+            time += " " + userSubmittedTime[2]+ " - " + userSubmittedTime[3];
+
+        }
+        course.setTime(time);
+
         return courseRepository.save(course);
     }
 
